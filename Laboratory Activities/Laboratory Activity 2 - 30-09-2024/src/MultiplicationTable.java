5a32b9c16e346142306d4630fcf4aae8ee707baf
kristaldimayuga <kcdimayuga651@gmail.com>
2024-09-30 09:34:05 +0800

    lab act 2 done

--- Laboratory Activities/Laboratory Activity 2 - 30-09-2024/src/MultiplicationTable.java
+++ Laboratory Activities/Laboratory Activity 2 - 30-09-2024/src/MultiplicationTable.java
@@ -10,17 +10,17 @@ public class MultiplicationTable {
         print_mtable(mtable, maxsize);
     }
 
-    public static void print_mtable(int[][] table, int max) {
+    public static void print_mtable(int[][] mtable, int max) {
         for (int i = 1; i <= max; i++) {
             for (int j = 1; j <= max; j++) {
-                table[i][j] = i * j;
+                mtable[i][j] = i * j;
             }
         }
 
         System.out.println("Multiplication Table:");
         for (int i = 1; i <= max; i++) {
             for (int j = 1; j <= max; j++) {
-                System.out.printf("%4d", table[i][j]);
+                System.out.printf("%4d", mtable[i][j]);
             }
             System.out.println();
         }
