4cfa23caf96da294680847f715ea2cc478e29425
kristaldimayuga <kcdimayuga651@gmail.com>
2024-11-27 18:24:00 +0800

    lab act 4 done

---
+++ Laboratory Activities/Laboatory Activity 4 - 27-11-2024/src/Shapes/ShapeCollection.java
@@ -0,0 +1,18 @@
+package Shapes;
+
+public class ShapeCollection {
+    private Shape[] shapes;
+
+    public ShapeCollection(Shape[] shapes){
+        this.shapes = shapes;
+    }
+
+    public void getPropertyValues(){
+        for (Shape shape : shapes){
+            shape.printShapeType();
+            System.out.println("Area: " + String.format("%.2f", shape.getArea()));
+            System.out.println("Perimeter: " + String.format("%.2f", shape.getPerimeter()));
+            System.out.println();
+        }
+    }
+}
